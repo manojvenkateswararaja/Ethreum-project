5fcbe831c845ddd5afd015a6c242dc6aeac1247d
manojvenkateswararaja <manoj.venkateswararaja@rapidqube.com>
2018-03-29 09:10:34 +0530

    changed

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -244,6 +244,8 @@ window.addEventListener('load', function() {
         console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        web3.eth.accounts.encrypt('a6739d2b49fbf189e927fc83d8a745b0a6067b9661a34c40713df597a0d0fe75', 'manoj')
+        web3.eth.accounts.decrypt(keystoreJsonV3, 'manoj');;
     }
 
       
